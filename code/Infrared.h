f090639947602864d0ac4f0195353b81e1118c52
HeeWoong97 <ungung97@LAPTOP-U0CO45L6>
2023-02-24 10:41:09 +0900

    feat : 충돌시 후진 및 충돌 기능 추가

--- code/Infrared.h
+++ code/Infrared.h
@@ -3,11 +3,16 @@
 
 #include "Motor.h"
 
+#define INFRARED_PIN    A4
+
 class Infrared : public Motor {
 private:
-
+    int state;
+    int curMotorSpeed;
 public:
-
+    Infrared();
+    void backUp(void);
+    void UTurn(void);
 };
 
 #endif
