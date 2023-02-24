8c3d09f227fb9ddfc6b7fdaf0021ffbf2f018aec
HeeWoong97 <ungung97@LAPTOP-U0CO45L6>
2023-02-24 10:44:31 +0900

    architecture : 폴더 이름 수정(code->SALT)

--- code/Motor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Motor.h"
-
-Motor::Motor() : motor_L(1), motor_R(4)
-{
-  motor_L.setSpeed(MOTOR_SPEED);
-  motor_L.run(RELEASE);
-  motor_R.setSpeed(MOTOR_SPEED);
-  motor_R.run(RELEASE);
-}
