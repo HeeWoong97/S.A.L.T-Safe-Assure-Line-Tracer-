3c3a43ba4e091a66af572eb40f7b86d3b288f5f4
taehyeong51 <107748183+taehyeong51@users.noreply.github.com>
2023-02-27 14:34:26 +0900

    feat : delay_ms() 함수 전달 인자 변경

--- SALT/Motor.cpp
+++ SALT/Motor.cpp
@@ -12,34 +12,34 @@ void Motor::turnLeft(void)
 {
   motor_L.run(BACKWARD);
   motor_R.run(FORWARD);
-  delay_ms(10);
+  delay_ms(MOTOR_DELAY);
   motor_L.run(RELEASE);
   motor_R.run(FORWARD);
-  delay_ms(10);
+  delay_ms(MOTOR_DELAY);
 }
 
 void Motor::turnRight(void)
 {
   motor_L.run(FORWARD);
   motor_R.run(BACKWARD);
-  delay_ms(10);
+  delay_ms(MOTOR_DELAY);
   motor_L.run(FORWARD);
   motor_R.run(RELEASE);
-  delay_ms(10);  
+  delay_ms(MOTOR_DELAY);  
 }
 
 void Motor::leftPivotTurn(void)
 {
   motor_L.run(BACKWARD);
   motor_R.run(FORWARD);
-  delay_ms(20);
+  delay_ms(MOTOR_DELAY);
 }
 
 void Motor::rightPivotTurn(void)
 {
   motor_L.run(FORWARD);
   motor_R.run(BACKWARD);
-  delay_ms(20);
+  delay_ms(MOTOR_DELAY);
 }
 
 void Motor::delay_ms(unsigned long milliseconds)
