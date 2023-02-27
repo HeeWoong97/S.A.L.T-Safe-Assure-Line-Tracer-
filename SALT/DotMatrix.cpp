62aaaed5a0aeac70198224da1ad8b7874b47a925
HeeWoong97 <ungung97@LAPTOP-U0CO45L6>
2023-02-27 09:58:36 +0900

    feat : 표정 추가

--- SALT/DotMatrix.cpp
+++ SALT/DotMatrix.cpp
@@ -1,15 +1,47 @@
 #include "DotMatrix.h"
 
+byte smile[8] = {
+  B00111100,
+  B01000010,
+  B10101001,
+  B10000101,
+  B10000101,
+  B10101001,
+  B01000010,
+  B00111100
+};
+
+byte sad[8] = {
+B00111100,
+B01000010,
+B10100101,
+B10001001,
+B10001001,
+B10100101,
+B01000010,
+B00111100
+};
+
 DotMatrix::DotMatrix() : lc(2, 10, 9, 1)
 {
   lc.shutdown(0, false);
   lc.setIntensity(0, 5);
   lc.clearDisplay(0);
+
+  shapes[SMILE] = smile;
+  shapes[SAD] = sad;  
 }
 
-void DotMatrix::display(byte shape[])
+void DotMatrix::displaySmile(void)
 {
   for (int i = 0; i < 8; i++) {
-    lc.setRow(0, i, shape);
+    lc.setRow(0, i, shapes[SMILE]);
+  }
+}
+
+void DotMatrix::displaySad(void)
+{
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(0, i, shapes[SAD]);
   }
 }
