97076461c25ae8d344d7d121e462aea979cf9cfb
HeeWoong97 <ungung97@LAPTOP-U0CO45L6>
2023-02-27 09:00:38 +0900

    feat : 음 재생 기능 추가

---
+++ SALT/Buzzer.h
@@ -0,0 +1,31 @@
+#ifndef _BUZZER_H_
+#define _BUZZER_H_
+
+#include <Arduino.h>
+
+#define BUZZER_PIN  13
+#define TEMPO       200
+
+#define C 262 // 도 
+#define D 294 // 레 
+#define E 330 // 미 
+#define F 349 // 파 
+#define G 392 // 솔 
+#define A 440 // 라 
+#define B 494 // 시 
+
+class Buzzer {
+private:
+
+public:
+  Buzzer();
+  void playDo(void);
+  void playRe(void);
+  void playMi(void);
+  void playFa(void);
+  void playSol(void);
+  void playRa(void);
+  void playSi(void);
+};
+
+#endif
